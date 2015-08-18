417a769d5a50af36a9d4ca8efbcb162e22da60a7
tcMichaelson <tc.michaelson@gmail.com>
2015-08-18 18:37:36 -0500

    Added homework for Day 1

---
+++ Contrarian/Contrarian/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contrarian {
+    class Program {
+        static void Main(string[] args) {
+
+            string response = "";
+            string contrary = "";
+            Console.Write("Enter a statement: ");
+            response = Console.ReadLine();
+            //int helpingIndex = getHelpVerbIndex(response);
+            //string notWord = getNotWord(response);
+            //if (helpingIndex > -1) {
+            //    if (notIndex > -1 && notIndex > helpingIndex) {
+            //        contrary = response.Replace(response.Substring())
+            //    }
+            //}
+            if (response.Contains("n't")) {
+                contrary = response.Replace("n't", "");
+            } else if (response.Contains(" not")) {
+                contrary = response.Replace("not", "");
+            } else {
+                contrary = response.Insert(response.IndexOf("like"), "don't ");
+            }
+            Console.WriteLine(contrary);
+            Console.ReadLine();
+
+
+        }
+        //static int getHelpVerbIndex(string str) {
+        //    string[] helpVerbs = {"am", "are", "is", "was", "were", "be", "being", "been",
+        //        "have", "has", "had","shall", "will","do", "does", "did","may", "must",
+        //        "might","can", "could", "would", "should" };
+        //    foreach (var text in helpVerbs) {
+        //        if (str.Contains(text)) {
+        //            return str.IndexOf(text);
+        //        }
+        //    }
+        //    return -1;
+        //}
+        //static string getNotWord(string str) {
+        //    string word = "";
+        //    if (str.Contains("not")) {
+        //        word = "not";
+        //    } else if (str.Contains("n't")) {
+        //        word = "n't";
+        //    }
+        //    return word;
+        //}
+
+    }
+}
---
+++ PlayMovieQuotes/PlayMovieQuotes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace PlayMovieQuotes {
+    class Program {
+        static void Main(string[] args) {
+            string varURL = "";
+            SoundPlayer quote = new SoundPlayer();
+            Random randNum = new Random();
+            int selectedQuote = (randNum.Next(1, 3));
+            switch (selectedQuote) {
+                case 1:
+                    varURL = "http://www.wavsource.com/snds_2015-08-16_6897529750891327/tv/sharknado/sharknado_afraid.wav";
+                    break;
+                case 2:
+                    varURL = "http://www.wavsource.com/snds_2015-08-16_6897529750891327/movies/misc/silence_lambs_dinner.wav";
+                    break;
+                case 3:
+                    varURL = "http://www.wavsource.com/snds_2015-08-16_6897529750891327/movies/aladdin/aladdin_cant_believe.wav";
+                    break;
+                default:
+                    break;
+            }
+            quote.SoundLocation = varURL;
+            if (quote == null) { }
+            try {
+                quote.Play();
+            } catch (Exception e) {
+                Console.WriteLine(@"I'm sorry, you are not connected to the internet.  Please reconnect and try again.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
