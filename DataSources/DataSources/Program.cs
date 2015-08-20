68026423977a463b593156554f251827d0c4c534
tcMichaelson <tc.michaelson@gmail.com>
2015-08-19 19:15:13 -0500

    CountWords class
    DataSources txt file

---
+++ DataSources/DataSources/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSources {
+    class Counter {
+        public static int CountWords(string str) {
+            return str.Split(' ').Length;
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            var words = Counter.CountWords("This is the longest string I've ever written!");
+            Console.WriteLine("The longest string you've ever written is {0} words long!", words);
+            Console.ReadLine();
+        }
+    }
+
+}
